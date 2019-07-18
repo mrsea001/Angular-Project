d402b8b6a45e0992103f549e0c0e2040f2ec0d30
mrsea001 <mrsea001@gmail.com>
2019-07-18 16:02:26 +0500

    Changed Title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularCli-App';
+  title = 'Angular-Project';
   visible=true;
   counter=0;
   myFavLang = [
